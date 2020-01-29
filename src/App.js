be8f5b08019727a1b77502ba45c4a0193a01638d
Philam <philamwild@gmail.com>
2020-01-29 17:02:17 +0100

    import revised components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import "./App.scss";
 import { Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import Equipment from "./pages/Equipment";
+import Shoes from "./pages/Shoes";
+import Textile from "./pages/Textile";
+import Ball from "./pages/Ball";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Switch></Switch>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/ball" component={Ball} />
+        <Route path="/equipment" component={Equipment} />
+        <Route path="/shoes" component={Shoes} />
+        <Route path="/textile" component={Textile} />
+      </Switch>
       <Footer />
     </div>
   );
