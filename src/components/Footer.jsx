ce4b751d36f5e8f662c2357dff7bdd7a7b4142d8
Philam <philamwild@gmail.com>
2020-01-29 17:58:25 +0100

    add content in footer jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import "./style/Footer.scss";
 
 export default function Footer() {
-  return <div>footer</div>;
+  return (
+    <div className="footer">
+      <div className="left-part">
+        <h3>Handball 4 Wilders</h3>
+        <ul>
+          <li>Qui sommes nous</li>
+          <li>Contact</li>
+          <li>Blabla</li>
+          <li>Blabla</li>
+        </ul>
+      </div>
+      <div className="right-part">
+        <h3>Abonnez-vous à la newsletter:</h3>
+        <input placeholder="Rentrer votre adresse email"></input>
+        <h3>Suivez nous</h3>
+        <i class="fa fa-facebook fa-3x"></i>
+        <i class="fa fa-twitter fa-3x"></i>
+        <i class="fa fa-instagram fa-3x"></i>
+        <i class="fa fa-youtube fa-3x"></i>
+        <i class="fa fa-linkedin fa-3x"></i>
+      </div>
+    </div>
+  );
 }
