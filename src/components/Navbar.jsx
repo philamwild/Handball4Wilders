c7dfa48af50b819ebecd234fd2a42ae0b953e6d5
Philam <philamwild@gmail.com>
2020-01-29 16:33:38 +0100

    add content in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import "./style/Navbar.scss";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="navbar-container">
+        <h1>Handball 4 Wilders</h1>
+        <ul>
+          <li>Chaussures</li>
+          <li>Ballons</li>
+          <li>Textile</li>
+          <li>Equipements</li>
+        </ul>
+        <div className="navbar-icons">
+          <i class="fa fa-user-circle fa-2x"></i>
+          <i class="fa fa-shopping-basket fa-2x"></i>
+        </div>
+      </div>
+    </div>
+  );
 }
