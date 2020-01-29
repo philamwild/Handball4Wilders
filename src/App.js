ebcaf7179cdccb8caa072cb06288409b5d22bc21
Philam <philamwild@gmail.com>
2020-01-29 15:16:59 +0100

    install router in app  and index js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.scss";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Switch></Switch>
+    </div>
+  );
 }
 
 export default App;
